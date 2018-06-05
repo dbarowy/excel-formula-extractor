deb8fbad6a2b40e94e693988d62fd1d6bc6981ec
Daniel W. Barowy <dbarowy@cs.williams.edu>
2018-06-05 14:00:03 -0400

    Extract and print formulas and addresses.

--- ExcelFormulaExtractor/ExcelFormulaExtractor/Ribbon1.cs
+++ ExcelFormulaExtractor/ExcelFormulaExtractor/Ribbon1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using Microsoft.Office.Tools.Ribbon;
+using System.Collections.Generic;
 using Application = Microsoft.Office.Interop.Excel.Application;
 using Workbook = Microsoft.Office.Interop.Excel.Workbook;
 
@@ -12,12 +15,31 @@ namespace ExcelFormulaExtractor
 
         }
 
+        private string prettyPrintFormulaDict(Dictionary<AST.Address,string> d)
+        {
+            var sb = new StringBuilder();
+            foreach (var kvp in d)
+            {
+                sb.Append(kvp.Key.A1Local());
+                sb.Append(" : ");
+                sb.Append(kvp.Value);
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
+
         private void extract_Click(object sender, RibbonControlEventArgs e)
         {
             Application app = Globals.ThisAddIn.Application.Application;
             Workbook wb = Globals.ThisAddIn.Application.ActiveWorkbook;
             var graph = new Depends.DAG(wb, app, ignore_parse_errors: false, dagBuilt: new DateTime());
-            System.Windows.Forms.MessageBox.Show("Dependence graph built");
+            var formulas =
+                graph
+                .getAllFormulaAddrs()
+                .Select(addr =>
+                    new Tuple<AST.Address, string>(addr, graph.getFormulaAtAddress(addr))
+                ).ToDictionary(tup => tup.Item1, tup => tup.Item2);
+            System.Windows.Forms.MessageBox.Show(prettyPrintFormulaDict(formulas));
         }
     }
 }
